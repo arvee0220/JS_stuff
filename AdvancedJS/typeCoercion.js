d0d1b8a167c9109f71cac1c438f2a8df86e2078b
Rowell Verdejo <rowellverdejo@gmail.com>
2023-03-21 20:12:24 +0800

    Upload exercises

---
+++ AdvancedJS/typeCoercion.js
@@ -0,0 +1,24 @@
+// Type Coercion = The language converting a certain type to another type
+
+// All programming languages have type coercion
+
+/* 
+	https://dorey.github.io/JavaScript-Equality-Table/
+	https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness
+	https://262.ecma-international.org/5.1/#sec-11.9.3
+
+
+*/
+
+1 == 1; // true
+1 == '1'; // true
+1 === 1; // true
+1 === '1'; // false
+1 == true; // true
+1 == false; // false
+1 === true; // false
+1 === false; // false
+0 == true; // false
+0 == false; // true
+0 === true; // false
+0 === false; // false
