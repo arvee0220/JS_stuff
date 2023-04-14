0282445f2d03f203dd4e0f017e3ce3cfe1d8fe10
Rowell Verdejo <rowellverdejo@gmail.com>
2023-04-14 19:49:02 +0800

    Test node import/export

---
+++ NodeJS/script.js
@@ -0,0 +1,11 @@
+const { largeNumber, smallNumber } = require("./script2.js");
+
+const a = smallNumber;
+const b = 5;
+
+// console.log(__dirname); // shows the current directory where cli is opened.
+
+console.log(a + b);
+
+// ES2020
+// globalThis === window; // true
---
+++ NodeJS/script2.js
@@ -0,0 +1,7 @@
+const largeNumber = 356;
+const smallNumber = 10;
+
+module.exports = {
+    largeNumber: largeNumber,
+    smallNumber: smallNumber,
+};
